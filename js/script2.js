8a6cd0a857f397bb099aecc48d537b67975e431a
Ivan <join127@mail.ru>
2021-11-15 20:42:42 +0300

    six commit

---
+++ js/script2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// let x = 5; alert( x++);
+
+// [] + false - null = true
+// console.log([] + false -null + true);  // "NaN"
+
+// let y = 1; 
+// let x = y = 2; 
+// alert(x);
+
+// console.log([] + 1 + 2); // "12"
+
+// alert( "1"[0] );
+
+// console.log(2 && 1 && null && 0 && underfined);
+// "И" запинается на лжи
+// "ИЛИ" запинается на правде
+// !!- булиновое значение
--- script1.js
+++ script1.js
@@ -214,3 +214,77 @@ const jonh = Object.create(soldier);
 
 // console.log(jonh.armor);
 jonh.sayHello();
+
+// To String
+
+// 1)
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
+
+// 2)
+console.log(typeof(5 + ''));
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fonSize = 26 + 'px';
+
+//To number
+
+//1)
+console.log(typeof(Number('4')));
+
+//2)
+console.log(typeof(+'5'));
+
+//3)
+console.log(typeof(parseInt("15px", 10)));
+
+
+let answ = +prompt("Hello", "");
+
+//To boolean
+
+// 0, '', null, underfined, NaN;
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+    console.log('Working...');
+}
+
+// 2)
+console.log(typeof(Boolean('4')));
+
+// 3)
+console.log(typeof(!!"44444"));
+
+// 
+
+const box = document.getElementById('box');
+
+console.log(box);
+
+const btns = document.getElementsByTagName('button')[1];
+
+console.log(btns[1]);
+
+const circles = document.getElementsByTagName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
+});
+
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
